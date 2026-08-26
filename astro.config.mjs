5393e6401a103a7a1e5f0ab11dffffa9d306bf62
cjhellwig <53386644+cjhellwig@users.noreply.github.com>
2026-08-26 16:51:58 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://whydevils.github.io',
+  site: 'https://cjhellwig.github.io',
   base: '/astro-scholar/',
 });
