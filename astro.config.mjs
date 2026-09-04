4e40e4c8be1ca4f9ba90145bcffcb9aa45234eab
cjhellwig <53386644+cjhellwig@users.noreply.github.com>
2026-09-04 19:07:02 -0400

    Update site URL and base path in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://cjhellwig.github.io',
-  base: '/astro-scholar/',
+  site: 'https://nyctutor.net',
+  base: '/',
 }); 
